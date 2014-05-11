a324102cc1dcd3d157930715257a12117e2fcd19
horiga <h.horiga@gmail.com>
2014-05-11 18:02:29 +0900

    no message

--- src/main/java/org/horiga/study/springboot/web/Application.java
+++ src/main/java/org/horiga/study/springboot/web/Application.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableAsync
+@EnableAsync // support asynchronous methods 
 public class Application {
 
     public static void main(String[] args) {
